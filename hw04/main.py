eea7a85bd8f1370f0525cd87a9d306c0c4e62242
hc328195244 <37677884+hc328195244@users.noreply.github.com>
2018-12-03 22:36:08 +0800

    Create hw04/main.py

---
+++ hw04/main.py
@@ -0,0 +1,35 @@
+import tornado.web
+import tornado.ioloop
+class the(tornado.web.RequestHandler):
+    def get(self,x):
+        x=int(x) if x is not None else 9
+        html = '''
+        <html>
+        <body>
+        <table>
+        '''
+        for i in range(1, x+1):
+            html +='<TR>'
+            for j in range(1, i+1):
+                html += '<TD>%d*%d=%d</TD>' % (j,i,i*j)
+            html +='</TR>'
+
+        html +='''
+        </html>
+        </body>
+        </table>
+        '''
+        self.write(html)
+
+
+
+
+
+        
+if __name__ == '__main__':
+    app=tornado.web.Application([
+        (r"(?:/([0-9])?)",the)
+    ]
+    )
+    app.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
